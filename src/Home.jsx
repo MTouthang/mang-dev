fd7f89843859c0881c5f5827348d9835eb19bcad
MTouthang <53576865+MTouthang@users.noreply.github.com>
2024-06-25 02:41:38 +0530

    initial set up

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import Layout from "./Layout/Layout";
+
+const Home = () => {
+  return (
+    <div>
+      <Layout>hello layout</Layout>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+
+// eslint-disable-next-line react/prop-types
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
