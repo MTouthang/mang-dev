0c9a874ee907be853a55f9ed4a09557e82cdb42b
MTouthang <53576865+MTouthang@users.noreply.github.com>
2024-09-02 23:57:30 +0530

    docs: project data

---
+++ src/data/data.js
@@ -0,0 +1,234 @@
+// experience data
+export const experiences = [
+  {
+    company: "Ineuron.ai",
+    role: "Associate Developer",
+    start: "May 2023",
+    end: "Nov 2023",
+    description: [
+      "Developing Curriculum and Content for Full Stack Development (MERN Stack)",
+      "Develop practical and engaging projects to help students apply their knowledge and build their skills in the MERN stack",
+      "Provide support and clear doubts to ensure students fully understand the concepts being taught",
+      "Work with the team to design and build capstone projects that integrate and apply the full range of skills learned by students",
+    ],
+  },
+  {
+    company: "Physics Wallah",
+    role: "Associate Full Stack Developer",
+    start: "Oct 2023",
+    end: "Present",
+    description: [
+      "Developed a structured and comprehensive curriculum that covers both fundamental and advanced concepts of full stack web development",
+      "Break down complex full stack web development concepts into easily digestible lessons to enhance student understanding",
+      "Create practical and straightforward projects that facilitate mentors in teaching and guiding students effectively",
+      "Perform thorough quality checks on recorded lecture videos to ensure clarity, accuracy, and completeness of the content",
+      "Work closely with the design team to ensure that the visual and user experience aspects align with the educational goals",
+      "Assist the team in seamlessly integrating the Sphere Engine to the Learning Management System (LMS) portal.",
+    ],
+  },
+];
+
+// projects data -
+export const projects = [
+  {
+    id: 1,
+    title: "Note keeping App",
+    image:
+      "https://camo.githubusercontent.com/769fba34ffe5b2bff31b215523b9fe432bb749cef90834494f248a4b8acf00a7/68747470733a2f2f6d65646961302e67697068792e636f6d2f6d656469612f61364b6e66304c67734c31724649437371552f67697068792e6769663f6369643d373930623736313137613835303066313033653331333966306566613432376162313636313564363162633837343338267269643d67697068792e6769662663743d67",
+    description:
+      "Built a full stack application similar to Google Keep. This project will involve creating a web application that allows users to take, organize, and manage notes efficiently. created with the MERN stack technolog½",
+    live: "https://noteskeeperweb.netlify.app/",
+    source: "https://github.com/MTouthang/notes-keeping-app",
+  },
+  {
+    id: 2,
+    title: "Language translator",
+    image:
+      "https://camo.githubusercontent.com/769fba34ffe5b2bff31b215523b9fe432bb749cef90834494f248a4b8acf00a7/68747470733a2f2f6d65646961302e67697068792e636f6d2f6d656469612f61364b6e66304c67734c31724649437371552f67697068792e6769663f6369643d373930623736313137613835303066313033653331333966306566613432376162313636313564363162633837343338267269643d67697068792e6769662663743d67",
+    description:
+      " Developed a React application utilizing the Memory translation API provided by Translated Labs. This app will translate text between multiple languages and provide a user friendly interface",
+    live: "https://noteskeeperweb.netlify.app/",
+    source: "https://github.com/sigma-webdev/language-translator-app",
+  },
+  {
+    id: 2,
+    title: "The Orion Academy",
+    image:
+      "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZHM4cnFicDFibXd6dmVjNHlxaDh6MnJjbnIzaTZhcGNta3E3MXU5bCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/FGHaAxHO3d0qQlDZLW/giphy.webp",
+    description: null,
+    live: "https://orian-school-of-arts-science.netlify.app/",
+    source: "https://github.com/MTouthang/orion-school/tree/main",
+  },
+  {
+    id: 3,
+    title: "Netflix clone",
+    image: null,
+    description:
+      " Developed a React application utilizing the Memory translation API provided by Translated Labs. This app will translate text between multiple languages and provide a user friendly interface",
+    live: null,
+    source: "https://github.com/sigma-webdev/netflix-clone",
+  },
+  {
+    id: 4,
+    title: "Pizza application",
+    image: null,
+    description: null,
+    live: "https://pizza-app-alpha-pink.vercel.app/",
+    source: "https://github.com/MTouthang/Pizza-app",
+  },
+  {
+    id: 5,
+    title: "Pokemon Card",
+    image:
+      "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExajg2YTFpMWh3bGJka3Bwb2FjanRmcndjOWhzbTY4ZWtxY3c4NWtzYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/OzJMbbNptfFdw7AjE2/giphy.webp",
+    description: " ",
+    live: "https://mellifluous-kringle-d34e4b.netlify.app/",
+    source:
+      "https://gist.github.com/MTouthang/760da1608704e673689153b72ee73523",
+  },
+  {
+    id: 6,
+    title: "Task todo ",
+    image: "",
+    description:
+      "Task-todo full fledged web application features that provided user authentication, add task, edit, delete, search and sort",
+    live: "https://tasktodosmanager.netlify.app/signup",
+    source: "https://github.com/MTouthang/task-todo",
+  },
+  {
+    id: 7,
+    title: "Weather App",
+    image: "",
+    description: " ",
+    live: "",
+    source: "",
+  },
+  {
+    id: 8,
+    title: "",
+    image: "",
+    description: " ",
+    live: "",
+    source: "",
+  },
+  {
+    id: 9,
+    title: "",
+    image: "",
+    description: " ",
+    live: "",
+    source: "",
+  },
+  {
+    id: 10,
+    title: "",
+    image: "",
+    description: " ",
+    live: "",
+    source: "",
+  },
+  {
+    id: 11,
+    title: "Paytm clone",
+    image: "",
+    description:
+      "A simple webpage clone of Paytm built with HTML and Tailwind CSS ",
+    live: "https://ecomclone101.netlify.app/",
+    source:
+      "https://github.com/MTouthang/tailwind-project/tree/main/paytm-clone",
+  },
+  {
+    id: 12,
+    title: "Shopify Clone",
+    image: "",
+    description:
+      "A simple webpage clone of Shopify built with html and Tailwind CSS",
+    live: "",
+    source: "",
+  },
+  {
+    id: 13,
+    title: "Shopify Clone",
+    image: "",
+    description:
+      "A simple webpage clone of Shopify built with html and Tailwind CSS",
+    live: "https://shopify101clone.netlify.app/",
+    source:
+      "https://github.com/MTouthang/tailwind-project/tree/main/shopify-clone",
+  },
+  {
+    id: 14,
+    title: "Rode clone",
+    image: "",
+    description:
+      "A simple webpage clone of Shopify built with html and Tailwind CSS",
+    live: "https://rodeclone101.netlify.app/",
+    source:
+      "https://github.com/MTouthang/tailwind-project/tree/main/rode-clone",
+  },
+  {
+    id: 15,
+    title: "Company API demo website",
+    image: "",
+    description:
+      "A simple webpage clone of Shopify built with html and Tailwind CSS",
+    live: "",
+    source: "",
+  },
+  {
+    id: 16,
+    title: "Hosting website",
+    image: "",
+    description:
+      "A simple webpage clone of Shopify built with html and Tailwind CSS",
+    live: "",
+    source: "",
+  },
+  {
+    id: 17,
+    title: "Business website",
+    image: "",
+    description:
+      "A simple webpage clone of Shopify built with html and Tailwind CSS",
+    live: "",
+    source: "",
+  },
+  {
+    id: 18,
+    title: "SAAS website",
+    image: "",
+    description:
+      "A simple webpage clone of Shopify built with html and Tailwind CSS",
+    live: "",
+    source: "",
+  },
+  {
+    id: 19,
+    title: "Visual Designer",
+    image: "",
+    description:
+      "A simple webpage clone of Shopify built with html and Tailwind CSS",
+    live: "",
+    source: "",
+  },
+];
+
+// readings
+export const readings = [
+  {
+    id: 1,
+    name: "The Pragmatic programmer",
+    description: "",
+    author: ["", ""],
+  },
+];
+
+// blogs
+export const blogs = [
+  {
+    id: 1,
+    title: "",
+    description: "",
+    source: "",
+  },
+];
