3192996307c511c060026b9a17db2f8ada29224c
MTouthang <53576865+MTouthang@users.noreply.github.com>
2024-09-07 23:25:02 +0530

    refactor: router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { Route, Routes } from "react-router-dom";
 import Home from "./Home";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Home />
     </>
   );
 }
